85fe24ca286e967207423dfef0d69fd69c1c97ea
1amb3 <a403481704@gmail.com>
2018-02-04 21:24:32 +0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-爬去蜻蜓fm的电台信息
+爬取蜻蜓fm的电台信息
+播放链接： http://lhttp.qingting.fm/live/+电台编号+/64k.mp3
+分类api： http://rapi.qingting.fm/categories?type=channel 
+分类底下电台的api： http://rapi.qingting.fm/categories/433/channels?with_total=true&page=1&pagesize=50
+认证
+```
+db.createUser({ user: 'lx0hacker', pwd: 'qwe123', roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ]});
+docker exec -it mongo mongo -u lx0hacker -p qwe123 --authenticationDatabase admin
+```
+创建用户：
+```
+use databasename
+db.createUser({user:"qtfm",pwd:"qwe123",roles:[{"role":"readWrite","db":"qingtingfm"}]})
+```
+python操作：
+https://api.mongodb.com/python/current/  
--- main.py
+++ main.py
@@ -1,2 +1,61 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
+
+import requests
+import pymongo
+import json
+import math
+import urllib.parse
+
+class QingtingFM(object):
+    def __init__(self,username,password):
+        self.username = urllib.parse.quote_plus(username)
+        self.password = urllib.parse.quote_plus(password)
+
+    def category_type(self):
+        r = requests.get('http://rapi.qingting.fm/categories?type=channel')
+        content = json.loads(r.text)
+        return content['Data']
+
+    def insert_db(self,title,description,address,category):
+        connection = pymongo.MongoClient('mongodb://{0}:{1}@127.0.0.1/qingtingfm?authMechanism=SCRAM-SHA-1'.format(self.username,self.password),27017)
+        db = connection.qingtingfm
+        post = db.radios
+        post.insert({'title':title,'descript':description,'address':address,'category':category})
+
+    def parse_data(self,datas,category):
+        for data in datas:
+            try:
+                title = data['title']
+                description = data['description']
+                address = 'http://lhttp.qingting.fm/live/'+str(data['content_id'])+'/64k.mp3'
+                category = category
+            except Exception as e:
+                title = data['title']
+                address = 'http://lhttp.qingting.fm/live/'+str(data['content_id'])+'/64k.mp3'
+                category = category
+                description = 'null'
+            finally:
+                self.insert_db(title,description,address,category)
+
+    def start(self):
+        datas = self.category_type()
+        count = 0
+        for data in datas:
+            r = requests.get(" http://rapi.qingting.fm/categories/"+str(data["id"])+"/channels?with_total=true")
+            total = json.loads(r.text)['Data']
+            total = total['total']
+            count+=total
+            
+            for num in range(1,math.ceil(total/12)+1):
+                r2 = requests.get('http://rapi.qingting.fm/categories/'+str(data["id"])+'/channels?with_total=true&page='+str(num)+'&pagesize=50')
+                items2 = json.loads(r2.text)['Data']['items']
+                self.parse_data(items2,data['title'])
+
+        print('一共爬取：',count)
+
+    
+if __name__ == '__main__':
+    qtfm = QingtingFM('qtfm','qwe123')
+    qtfm.start()
+    print('done')
